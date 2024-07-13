6667f5b81d34634b05ec8b874f44b1920ea3a4b4
Degraft Frimpong <frimpsup@gmail.com>
2024-07-13 17:44:34 -0400

    env changed

--- app/lib/s3.server.ts
+++ app/lib/s3.server.ts
@@ -11,7 +11,7 @@ const s3 = new S3Client({
 	endpoint,
 	credentials: {
 		accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
-		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+		secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY as string,
 	},
 	region: process.env.AWS_REGION as string,
 });
